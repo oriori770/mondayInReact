dbc84020629805ba83bb4bd296e31eccc6b2b250
oriori770 <oriori770@gmail.com>
2024-10-28 17:52:57 +0200

    add users

--- src/component/Users/Users.tsx
+++ src/component/Users/Users.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import OneUser from '../User'
 
 type User = {
@@ -9,6 +9,8 @@ type User = {
 
 const Users = () => {
     const [users, setusers] = useState<User[]>([])
+    const newName = useRef<HTMLInputElement>(null)
+    const newEmail = useRef<HTMLInputElement>(null)
     useEffect(() => {
         setusers([{id: 1, name: 'test', email: 'ori@gmail.com'}, {id: 2, name: 'jon doe', email: 'test@gmail.com'}, {id: 3, name: 'shneor', email: 'test'}])
     },[])
@@ -16,6 +18,9 @@ const Users = () => {
     <>
     <div>add user</div>
 
+    <input type="text" ref={newName} />
+    <input type="text" ref={newEmail} />
+    <button onClick={() => {if(newName.current && newEmail.current) {setusers([...users, {id: users.length+1, name: newName.current?.value, email: newEmail.current?.value}])}}}>add</button>
          {users.map((user) => <OneUser key={user.id} id={user.id} name={user.name} email={user.email}/>) }
     </>
   )
